01db88992a138ddd3a9e326d73955e334ed3e7b7
Amir Gueddaoui <amirg@scrumlogic.com>
2018-12-24 10:10:07 +0300

    Try to eject project.
    Need to commit changes.

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+describe('Redux get news', () => {
+  it('Redux test 1', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('Redux test 2', () => {
+    expect(true).toBeTruthy();
+  });
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
